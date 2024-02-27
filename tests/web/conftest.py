a17ec01c0a28451294948c6caf5848bf0f529961
DianaValieva <didarphin@yandex.ru>
2024-02-27 11:29:40 +0300

    Add selenoid options

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,9 +1,29 @@
 import pytest
 from selene import browser
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="function",autouse=True)
+
+@pytest.fixture(scope="function", autouse=True)
 def set_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        },
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options,
+    )
+
+    browser.config.driver = driver
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.driver.maximize_window()
